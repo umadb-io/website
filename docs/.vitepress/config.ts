3398f96a6fcd9a0c7d9911809a28fff07874eaa4
johnbywater <john.bywater@appropriatesoftware.net>
2025-11-27 22:02:36 +0000

    Adjusted nav and description in config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
     title: 'UmaDB',
-    description: 'A very basic VitePress website',
+    description: 'Open-source gRPC event store server built for Dynamic Consistency Boundaries in Rust',
     themeConfig: {
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Key Features', link: '/key-features' },
-            { text: 'Core Concepts', link: '/core-concepts' },
+            { text: 'Features', link: '/key-features' },
+            { text: 'Concepts', link: '/core-concepts' },
             { text: 'Architecture', link: '/architecture' },
             { text: 'Benchmarks', link: '/benchmarks' },
             { text: 'Installing', link: '/installing' },
             { text: 'Running', link: '/running' },
             { text: 'Docker', link: '/docker' },
-            { text: 'gRPC API', link: '/grpc-api' },
+            { text: 'API', link: '/grpc-api' },
             { text: 'Rust', link: '/rust-client' },
             { text: 'Python', link: '/python-client' },
             { text: 'Developers', link: '/developers' },
