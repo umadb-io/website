9bc6a01baeba6cf1d1e1435da1941936fe05ef62
johnbywater <john.bywater@appropriatesoftware.net>
2025-11-30 22:31:06 +0000

    Updated header links (for favicons).

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -82,8 +82,11 @@ export default defineConfig({
         ['link', { rel: 'canonical', href: 'https://umadb.io' }],
 
         // --- Favicon & PWA icons ---
-        ['link', { rel: 'icon', href: '/favicon.ico' }],
-        ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: "image/png", sizes: "16x16", href: '/favicon-16x16.png' }],
+        ['link', { rel: 'icon', type: "image/png", sizes: "32x32", href: '/favicon-32x32.png' }],
+        ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+        ['link', { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' }],
+        ['link', { rel: 'manifest', href: '/site.webmanifest' }],
 
         // --- Open Graph (Facebook / Slack / Discord previews) ---
         ['meta', { property: 'og:title', content: 'UmaDB' }],
