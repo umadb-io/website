bce276b70d17e0677041a81237d982212bea12d1
johnbywater <john.bywater@appropriatesoftware.net>
2025-12-30 01:17:25 +0000

    Removed all but one icon link (favicon is not appearing in Google search results).

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -95,13 +95,7 @@ export default defineConfig({
         ['link', { rel: 'canonical', href: 'https://umadb.io' }],
 
         // --- Favicon & PWA icons ---
-        ['link', { rel: 'icon', type: "image/png", sizes: "16x16", href: '/favicon-16x16.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: "32x32", href: '/favicon-32x32.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: "48x48", href: '/favicon-48x48.png' }],
         ['link', { rel: 'icon', type: "image/png", sizes: "96x96", href: '/favicon-96x96.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: "144x144", href: '/favicon-144x144.png' }],
-        ['link', { rel: 'icon', type: "image/png", sizes: "152x152", href: '/favicon-152x152.png' }],
-        ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
         ['link', { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' }],
         ['link', { rel: 'manifest', href: '/site.webmanifest' }],
 
